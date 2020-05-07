e606b3e1cf06036e0a67075492353fdc6ad88d9d
matenemeth-DESKTOP <mate.nemeth@outlook.hu>
2020-05-07 20:34:17 +0200

    build

---
+++ dist/reducer/action.types.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.actionTypes = void 0;
+var actionTypes = {
+  ADD_EDIT: 'ADD_EDIT',
+  ORDER_BY: 'ORDER_BY',
+  POPULATE_DATA: 'POPULATE_DATA'
+};
+exports.actionTypes = actionTypes;
